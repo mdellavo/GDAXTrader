1da23f0dab1977a7adec315a33e6d9cc8e37e2ef
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2018-02-04 12:17:36 -0500

    welcome card

--- app/src/main/java/org/quuux/gdax/fragments/HomeFragment.java
+++ app/src/main/java/org/quuux/gdax/fragments/HomeFragment.java
@@ -2,7 +2,8 @@ package org.quuux.gdax.fragments;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,11 @@ import android.view.ViewGroup;
 import org.quuux.gdax.R;
 
 public class HomeFragment extends BaseGDAXFragment {
+
+    RecyclerView mRecyclerView;
+    LinearLayoutManager mLayoutManager;
+     HomeAdapter mAdapter;
+
     public HomeFragment() {
     }
 
@@ -18,6 +24,11 @@ public class HomeFragment extends BaseGDAXFragment {
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public boolean needsProductSelector() {
+        return true;
     }
 
     @Override
@@ -31,7 +42,82 @@ public class HomeFragment extends BaseGDAXFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
+        mRecyclerView = v.findViewById(R.id.recycler_view);
+
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new HomeAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+
         return v;
+    }
+
+    private boolean shouldShowWelcomeCard() {
+        return true;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(final View itemView) {
+            super(itemView);
+        }
+
+        void bind() {
+
+        }
+    }
+
+    class WelcomeCard extends ViewHolder {
+        public WelcomeCard(final View itemView) {
+            super(itemView);
+        }
+    }
+
+    final static int CARD_HOME = 0;
+    final static int CARD_ACTIVITY = 1;
+
+    class HomeAdapter extends RecyclerView.Adapter<ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+
+            ViewHolder rv = null;
+            switch (viewType) {
+                case CARD_HOME:
+                    rv = new WelcomeCard(inflater.inflate(R.layout.card_welcome, parent, false));
+                    break;
+            }
+
+            return rv;
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
+            holder.bind();
+        }
+
+        @Override
+        public int getItemCount() {
+            int num = 0;
+
+            if (shouldShowWelcomeCard())
+                num += 1;
+
+            return num;
+        }
+
+        @Override
+        public long getItemId(final int position) {
+            return position;
+        }
+
+        @Override
+        public int getItemViewType(final int position) {
+            return position;
+        }
     }
 
 }
